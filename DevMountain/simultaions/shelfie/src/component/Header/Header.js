73158f90b89365c7fbbaad962d31d81ebad6eba3
Andres <thimgana@gmail.com>
2019-02-04 00:08:08 -0700

    set up repository

---
+++ DevMountain/simultaions/shelfie/src/component/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header (props){
+    return(
+        <div>
+            Header{props.Header}
+        </div>
+         )
+};
+
+
+
+export default Header;
---
+++ DevMountain/simultaions/shelfie/src/component/Product/Product.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Product (props){
+    return(
+        <div>
+            Product{props.Product}
+        </div>
+         )
+};
+
+
+
+export default Product;
